d6adc0cac04873cfadfd80d56cf2cdfaf8fa399c
Janhavi Mishra <86625600+Janhavi-Mishra@users.noreply.github.com>
2022-04-15 21:47:06 +0530

    Update main.py

--- Day - 10: Calculator/main.py
+++ Day - 10: Calculator/main.py
@@ -1,6 +1,18 @@
 #Project - calculator
 import art
 
+class clear:
+ def __call__(self):
+  import os
+  if os.name==('ce','nt','dos'): os.system('cls')
+  elif os.name=='posix': os.system('clear')
+  else: print('\n'*120)
+ def __neg__(self): self()
+ def __repr__(self):
+  self();return ''
+
+clear=clear()
+
 #add
 def add(n1, n2):
     return n1 + n2
